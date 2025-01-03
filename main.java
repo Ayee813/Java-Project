1bdae545193afe56b942a6425470c9da65aaecdd
YongYee Vue <yongyeelove813@gmail.com>
2025-01-03 22:39:26 +0700

    second commit

--- main.java
+++ main.java
@@ -4,9 +4,15 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         Scanner scan = new  Scanner(System.in);
-        
-        System.err.println("Enter your choice: ");
+        System.out.print("1. Area calculator");
+        System.out.print("2. Equation calculator");
+        System.out.print("3. Volume calculator");
+        System.err.print("Enter your choice: ");
         String choice  = scan.nextLine();
+        if (choice == 1 || choice ==  "1") {
+            squuare_Area();            
+        } else{
+            System.out.print("invied");
+        }
     }
-
 }
