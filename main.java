7b73b610e9cc1f8b49dd32bf6890d876ff6d1976
YongYee Vue <yongyeelove813@gmail.com>
2025-01-03 22:07:13 +0700

    first

---
+++ main.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scan = new  Scanner(System.in);
+        
+        System.err.println("Enter your choice: ");
+        String choice  = scan.nextLine();
+    }
+
+}
